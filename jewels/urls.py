3fcacff7d26360e52eb2e7effd6f57e9d890e608
Samuelosondu99@gmail.com <Samuelosondu99@gmail.com>
2022-11-10 12:57:44 +0100

    Commit

--- jewels/urls.py
+++ jewels/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('jewellery/', views.jewellery, name='jewellery'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_request, name='login'),
     path('register/', views.register, name='register'),
+    path("logout", views.logout_request, name="logout"),
 ]
--- jewels/views.py
+++ jewels/views.py
@@ -2,28 +2,71 @@ from django.shortcuts import render
 from .models import Item
 # Create your views here.
 
+from django.contrib.auth.forms import AuthenticationForm #add this
+from django.contrib.auth import login, authenticate, logout  # add this
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+
+def register(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registration successful." )
+			return redirect("jewels/index.html")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="jewels/register.html", context={"register_form":form})
+
+
+def login_request(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("/jewels")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request, 'jewels/login.html', context={"login_form":form})
+
+
+def logout_request(request):
+	logout(request)
+	messages.info(request, "You have successfully logged out.")
+	return redirect("/jewels")
+
 
 def index(request):
-    items = Item.objects.all()
-
-    return render(request, 'jewels/index.html', {'user': items})
+	items = Item.objects.all()
+	return render(request, 'jewels/index.html', {'user': items})
 
 
 def about(request):
-    return render(request, 'jewels/about.html')
+	return render(request, 'jewels/about.html')
 
 
 def contact(request):
-    return render(request, 'jewels/contact.html')
+	return render(request, 'jewels/contact.html')
 
 
 def jewellery(request):
-    items = Item.objects.all()
-    return render(request, 'jewels/jewellery.html', {'items': items})
-
-def login(request):
-    return render(request, 'jewels/login.html')
+	items = Item.objects.all()
+	return render(request, 'jewels/jewellery.html', {'items': items})
 
 
-def register(request):
-    return render(request, 'jewels/register.html')
+
+
+
+# def register(request):
+#     return render(request, 'jewels/register.html')
